ffed727c77e2c04a43b01b612e65afa8e1d46514
Genaro10020 <iscgenarovp@gmail.com>
2022-04-20 09:27:05 -0500

    titulo de introduccion a Videos

--- video_actividades.php
+++ video_actividades.php
@@ -16,7 +16,7 @@ if($tipo=="capacitacion" || $tipo=="videos"){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Introducción</title>
+    <title>Videos</title>
     <!--CSS 5-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -171,7 +171,7 @@ const app = {
        console.log(this.tipo_solicitud);
        console.log(this.video_solicitado)
        if(this.tipo_solicitud=="capacitacion"){
-        document.getElementById("verificar").removeAttribute("controls");
+        //document.getElementById("verificar").removeAttribute("controls");
        }
        if(this.video_solicitado=="introduccion"){
             this.titulo="INTRODUCCIÓN";
